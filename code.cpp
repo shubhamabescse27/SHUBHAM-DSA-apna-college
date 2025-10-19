99c35d6cb87f59636df48677d550bbf786370934
shubham jaiswal <sj430149@gmail.com>
2025-10-19 16:30:01 +0530

    odd even case

--- code.cpp
+++ code.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main() { 
-int age;
-cout <<"inter the age :";
-cin >> age;
-
-if(age >= 18) {
-    cout <<"you can vote\n";
-} else {
-    cout << "you can not vote\n";
+int n;
+cout <<"enter the number : ";
+cin >> n;
+if(n%2 == 0){
+    cout <<"n is even\n";
 }
+
+    else{
+        cout <<"n is odd\n";
+    }
 
 return 0;
 }
