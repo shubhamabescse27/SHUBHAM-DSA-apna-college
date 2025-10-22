04b2e25c9d74a3b92210910916de882eb56aad7f
shubham jaiswal <sj430149@gmail.com>
2025-10-22 23:23:18 +0530

    ques

--- code.cpp
+++ code.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() { 
-  int n= 50;
-  int sum = 0;
-
-  for(int i=1; i<=n; i++) {
-   sum += i;
-   if(i == 5) {
-      break;
+int main() {
+   int n = 5;
+   // print odd numbers
+   for(int i=1; i<=n; i++){
+      if(i%2 != 0){
+      cout << i << " ";
+      }
    }
-
-
-  }
-cout << "sum = " << sum << endl;
-
+  cout << endl; 
 return 0;
 }
